4b1a7236e31edd13850043e7f0ab8d1a9e529296
Florian Loch <florian.loch@gmail.com>
2017-05-21 15:45:55 +0200

    Enhance Marquee massively, can now handle long strings by substringing the visible part

--- src/Marquee.h
+++ src/Marquee.h
@@ -3,37 +3,57 @@
 class Marquee {
 private:
   uint16_t offset = 0;
-  uint16_t widthOfText;
+  uint8_t fontWidth;
   uint16_t screenWidth;
   bool toTheRight = false;
   uint16_t diff;
-public:
-  Marquee(uint16_t screenWidth) : screenWidth{screenWidth} {};
+  String text;
+  bool dontScroll;
 
-  void setWidthOfText(uint16_t widthOfText) {
-    this->widthOfText = widthOfText;
-    diff = widthOfText - screenWidth;
-  }
-
-  uint16_t move() {
-    if (widthOfText > screenWidth) {
-      if (!toTheRight) {
-        offset++;
-        if (offset == diff) {
-          toTheRight = true;
-        }
+  uint16_t _scroll() {
+    if (!toTheRight) {
+      offset++;
+      if (offset == diff) {
+        toTheRight = true;
       }
-      else {
-        offset--;
-        if (offset == 0) {
-          toTheRight = false;
-        }
+    }
+    else {
+      offset--;
+      if (offset == 0) {
+        toTheRight = false;
       }
-
-      return offset;
     }
 
-    return 0;
+    return offset;
+  };
+public:
+  Marquee(uint16_t screenWidth, uint8_t fontWidth, String text)
+  : screenWidth{screenWidth},
+    fontWidth{fontWidth},
+    text{text}
+  {
+    diff = text.length() * fontWidth - screenWidth;
+    dontScroll = text.length() * fontWidth < screenWidth;
+  };
+
+  Marquee(uint16_t screenWidth, uint8_t fontWidth) {
+    String emptyStr("");
+    Marquee(screenWidth, fontWidth, emptyStr);
+  };
+
+  uint8_t scroll() {
+    if (dontScroll) {
+      return 0;
+    }
+
+    return _scroll() % fontWidth;
+  }
+
+  String getVisibleSubstring() {
+    uint16_t charsToCutOffAtStart = offset / fontWidth;
+    uint16_t lastCharNeeded = charsToCutOffAtStart + (offset + screenWidth) / fontWidth + 1 + 1;
+
+    return text.substring(charsToCutOffAtStart, lastCharNeeded);
   };
 
   Marquee& reset() {
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,12 @@
 #include "Decoder.h"
 #include "Marquee.h"
 
+#define FONT u8g2_font_7x14_tf
+#define FONT_WIDTH 7
+#define FONT_HEIGHT 14
+
+void drawInfo(uint8_t line, Marquee& marquee);
+
 U8G2_SSD1306_128X64_NONAME_F_4W_SW_SPI u8g2(U8G2_R0, /* clock,scl=*/ 14, /* data,sda=*/ 13, /* (NOT USED) cs=*/ U8X8_PIN_NONE, /* dc=*/ 4, /* reset=*/ 5);
 //Working with I2C seems not to work, also when soldering the jumpers on the display
 //U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, 5, 4);
@@ -17,9 +23,11 @@ const String ssid     = WIFI_SSID;
 const char* password = WIFI_KEY;
 
 trackInformation_t currentTrack;
-Marquee marqueeTitle(SCREEN_WIDTH);
-Marquee marqueeArtist(SCREEN_WIDTH);
-Marquee marqueeAlbum(SCREEN_WIDTH);
+String noInfoYet("No track information received yet... Waiting...");
+String empty = "";
+Marquee marqueeTitle(SCREEN_WIDTH, FONT_WIDTH, noInfoYet);
+Marquee marqueeArtist(SCREEN_WIDTH, FONT_WIDTH, empty);
+Marquee marqueeAlbum(SCREEN_WIDTH, FONT_WIDTH, empty);
 
 Decoder decoder("secret");
 
@@ -27,9 +35,9 @@ UDPReceiver udp(47000, [&](uint8_t* buffer, size_t size, String remoteAddress, u
   Serial.println("Received UDP package from " + remoteAddress + ":" + remotePort);
 
   currentTrack = decoder.decode(buffer, size);
-  marqueeTitle.reset().setWidthOfText(u8g2.getStrWidth(currentTrack.title.c_str()));
-  marqueeArtist.reset().setWidthOfText(u8g2.getStrWidth(currentTrack.artist.c_str()));
-  marqueeAlbum.reset().setWidthOfText(u8g2.getStrWidth(currentTrack.album.c_str()));
+  marqueeTitle = Marquee(SCREEN_WIDTH, FONT_WIDTH, currentTrack.title);
+  marqueeArtist = Marquee(SCREEN_WIDTH, FONT_WIDTH, currentTrack.artist);
+  marqueeAlbum = Marquee(SCREEN_WIDTH, FONT_WIDTH, currentTrack.album);
 
   Serial.println("Playing '" + currentTrack.title + "' from '" + currentTrack.artist + "' on album '" + currentTrack.album + "'");
 });
@@ -38,11 +46,12 @@ void setup(void) {
   Serial.begin(9600);
   u8g2.begin();
 
-  // text display tests
+  u8g2.setFont(FONT);
+
   u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_profont12_tf);
-  u8g2.drawStr(0, 10, "Connecting to WiFi:");
-  u8g2.drawStr(0, 28, shorten("SSID: " + ssid).c_str());
+
+  u8g2.drawStr(0, FONT_HEIGHT, "Connecting to WiFi");
+  u8g2.drawStr(0, 15 + FONT_HEIGHT, shorten("SSID: " + ssid).c_str());
 
   u8g2.sendBuffer();
 
@@ -68,15 +77,22 @@ void setup(void) {
 void loop(void) {
   u8g2.clearBuffer();
 
-  u8g2.setFont(u8g2_font_profont12_tf);
-
-  u8g2.drawStr(-marqueeTitle.move(), 8, currentTrack.title.c_str());
-  u8g2.drawStr(-marqueeArtist.move(), 28, currentTrack.artist.c_str());
-  u8g2.drawStr(-marqueeAlbum.move(), 48, currentTrack.album.c_str());
+  drawInfo(0, marqueeTitle);
+  drawInfo(1, marqueeArtist);
+  drawInfo(2, marqueeAlbum);
 
   u8g2.sendBuffer();
 
   udp.check();
   yield();
   delay(10);
+}
+
+void drawInfo(uint8_t line, Marquee& marquee) {
+  uint8_t offset = marquee.scroll();
+  const String vs = marquee.getVisibleSubstring();
+
+  uint8_t y = line > 0 ? 20 + line * FONT_HEIGHT : FONT_HEIGHT;
+
+  u8g2.drawStr(-offset, y, vs.c_str());
 }
